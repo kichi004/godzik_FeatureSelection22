6bdc32332a46bf6453cfd9911f7994bb93422c6e
kichi004 <100912526+kichi004@users.noreply.github.com>
2023-01-07 09:33:32 -0800

    added greedy forward selection search

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import ordered_feature_selector
 import loocv_accuracy
+import feature_importances_list
 
-# print(loocv_accuracy.find_accuracy("_imputed_data.csv"))
+print(loocv_accuracy.find_accuracy("_greedy_final.csv"))
+# names = ['Sex','Hispanic','Diabetes','Hypertension','Neutrophils','Monocytes','B cells','MHCII Monocytes','Resistin','IL-8','IP-10','IL-6','IFNλ2/3','Platelets','MHCII+Platelets','LCN2','Myoglobin','CRP','OPN','MPO','ICAM-1','VCAM-1','Cystatin C','D-dimer']
+# print(feature_importances_list.avg_feature_importances("_imputed_data.csv", names, 10000))
--- n_estimators_search.py
+++ n_estimators_search.py
@@ -6,21 +6,20 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 
 # load dataset into pandas dataframe
-df = pd.read_csv("imputed.csv")
+df = pd.read_csv("_greedy_final1.csv")
 
 # split dataset into label columns and feature columns
-feats = df.drop(['0'], axis = 1) # everything but the first column
-labels = df['0'] # just the first column
+feats = df.drop(['Group'], axis = 1) # everything but the first column
+labels = df['Group'] # just the first column
 
 # define parameters for possible n_estimator values
-trees_parameters = {'n_estimators': [10, 20, 30, 40, 50, 60]}
+trees_parameters = {'n_estimators': [64, 80, 96, 112, 128]}
 best_params_list = []
 
 # loops the following 
 for i in range(100):
     # initialize random forest classifier model
     rfc_model = RandomForestClassifier()
-
     # initialize grid search object
     grid_search = GridSearchCV(rfc_model, trees_parameters, cv = 10, scoring = 'accuracy')
 
@@ -30,6 +29,7 @@ for i in range(100):
 
 # calculate the average of the best_params_list
 print(f'Average Best n_estimator value: {statistics.mean(best_params_list):.1f} estimators')
+print(best_params_list)
 
 # currently using 30 estimators based on results
 
