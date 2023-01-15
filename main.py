d05dc889f1627de5ef5afa4003c4844f7f96b0c9
kichi004 <100912526+kichi004@users.noreply.github.com>
2023-01-14 16:19:10 -0800

    renamed and added greedy search choice

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import imputer
 import accuracy_finder
+import greedy_feature_searcher
 
 while True:
-    choice_input = input("\nEnter the value for the desired task.\n2. Impute | 3. Accuracy | \n")
+    choice_input = input("\nEnter the value for the desired task.\n2. Impute | 3. Accuracy | 4. Greedy Search | 5. Importances | 6. Importance Search | 7. Visualize\n")
 
     if choice_input == '1': # everything >> imputes, greedy search and importance search
         print('hello')
         break
 
     if choice_input == '2': # imputes file
-
         # select file
         file_input = input("Enter the name of the CSV file: ")
 
@@ -17,7 +17,9 @@ while True:
         version_number = input("Enter the version number of the file: ")
 
         selected_columns = []
-        if version_number == '1' or version_number == 'v1':
+        if version_number == '0' or version_number == 'v0':
+            selected_columns == ['Resistin', 'IL-6', 'IFNλ2/3', 'OPN', 'Cystatin C', 'D-dimer']
+        elif version_number == '1' or version_number == 'v1':
             selected_columns = ['Resistin', 'OPN', 'D-dimer']
         elif version_number == '2' or version_number == 'v2':
             selected_columns = ['Resistin']
@@ -34,7 +36,6 @@ while True:
         break
 
     if choice_input == '3': # find accuracy
-
         # select file
         file_input = input("Enter the name of the CSV file: ")
 
@@ -53,6 +54,27 @@ while True:
 
         # call function
         accuracy_finder.find_accuracy_trials(file_input, trial_input, depth_input)
+        break
+
+    if choice_input == '4': # greedy forward selection
+        # select file
+        file_input = input("Enter the name of the CSV file: ")
+
+        # select printing threshold
+        threshold_input = input("Print any combinations of accuracy higher than what value?: ")
+        threshold_input = int(threshold_input)
+        
+        # select max depth
+        depth_input = input("Enter the maximum depth for the classifier (enter '0' for no maximum): ")
+        depth_input = int(depth_input)
+        if depth_input == 0:
+            depth_input = None
+
+        # add spacing
+        print()
+
+        # call function
+        greedy_feature_searcher.greedy_fw_search(file_input, threshold_input, depth_input)
         break
 
     else:
