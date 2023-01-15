c4d55d8291abfb572607ecd3a246130e6e7996c7
kichi004 <100912526+kichi004@users.noreply.github.com>
2023-01-14 17:03:21 -0800

    moved functions from feature_importances_list.py

--- importance_feature_selector.py
+++ importance_feature_selector.py
@@ -44,7 +44,40 @@ def sorted_df(file_name, n = 500, depth_input = None):
     sorted_df.to_csv("_sorted_features.csv", index = False)
 
 # print importances list + get top n
+def avg_feature_importances(file_name, feature_names, trials = 100):
+    # initialize empty list to store importances
+    importances_list = []
 
+    # load dataset
+    df = pd.read_csv(file_name)
+
+    # split dataset into label columns and feature columns
+    feats = df.drop(['Group'], axis = 1) # everything but the first column
+    labels = df['Group'] # just the first column
+
+    # loop to run multiple trials
+    for i in range(trials):
+
+        # Create a random forest classifier
+        rfc = RandomForestClassifier()
+
+        # fit classifier to the data
+        rfc.fit(feats, labels)
+
+        # get feature importances
+        importances = rfc.feature_importances_
+
+        # append importances to the list
+        importances_list.append(importances)
+
+    # convert list to numpy array
+    importances_array = np.array(importances_list)
+
+    # Compute mean of importances
+    mean_importances = np.mean(importances_array, axis=0)
+
+    for name, importance in zip(feature_names, mean_importances):
+        print(f'{name}: {importance:.3f}')
 
 # search through top n's function
 def best_n_features_search(file_name, trials_input = 20, depth_input = None):
